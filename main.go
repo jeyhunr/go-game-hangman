9c51da1fd972d3ac078cf8dd70c5012acfdaa3aa
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2024-06-12 20:04:39 +0200

    add: initializeGuessedWord anf printGameState functions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"unicode"
 )
 
 var dictionary = []string{
@@ -18,10 +19,34 @@ var dictionary = []string{
 
 func main() {
 	targetWord := getRandomWord()
+	guessedLetters := initializeGuessedWord(targetWord)
+	printGameState(targetWord, guessedLetters)
 
-	fmt.Println(targetWord)
+}
+
+func initializeGuessedWord(targetWord string) map[rune]bool {
+	guessedLetters := map[rune]bool{}
+	guessedLetters[unicode.ToLower(rune(targetWord[0]))] = true
+	guessedLetters[unicode.ToLower(rune(targetWord[len(targetWord)-1]))] = true
+
+	return guessedLetters
 }
 
 func getRandomWord() string {
 	return dictionary[rand.Intn(len(dictionary))]
+}
+
+func printGameState(targetWord string, guessedLetters map[rune]bool) {
+	for _, char := range targetWord {
+		if char == ' ' {
+			fmt.Print(" ")
+		} else if guessedLetters[unicode.ToLower(char)] {
+			fmt.Printf("%c", char)
+		} else {
+			fmt.Print("_")
+		}
+		fmt.Print(" ")
+	}
+
+	fmt.Println()
 }
